5d54b8cd072ee2982554f226581053db9bdda9d5
asakhant <asakhantayev@gmail.com>
2024-06-15 15:09:01 +0500

    Mode choosing with input validation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module crunch02
+
+go 1.20
+
+require github.com/alem-platform/ap v0.1.3
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alem-platform/ap"
+)
+
+func main() {
+	PrintString("Hello! This is Minesweeper.\n")
+	PrintString("You can create your own map or play a generated one.\n\n")
+
+	MODE := ChooseMode()
+
+	fmt.Println(MODE)
+
+	if MODE == 1 {
+	} else {
+	}
+}
+
+func ReadHeightAndWidth() (int, int) {
+	var height, width int
+
+	_, err := fmt.Scanf("%d %d", &height, &width)
+	if err != nil || height < 3 || width < 3 {
+		return -1, -1
+	}
+
+	return height, width
+}
+
+func ChooseMode() int {
+	PrintString("Choose a mode:\n1. Enter a custom map\n2. Generate a random map\n")
+	PrintString("Enter your choice (1/2): ")
+
+	var mode int
+
+	var inp string
+	var r rune
+	fmt.Scanf("%c", &r)
+	if r == '\n' {
+		PrintString("Sorry, you have entered an invalid option. Please choose 1 or 2.\n\n")
+
+		return ChooseMode()
+	}
+	for r != '\n' {
+		inp += string(r)
+		fmt.Scanf("%c", &r)
+	}
+
+	if len(inp) != 1 || !(inp[0] == '1' || inp[0] == '2') {
+		PrintString("Sorry, you have entered an invalid option. Please choose 1 or 2.\n\n")
+
+		return ChooseMode()
+	}
+
+	return mode
+}
+
+func PrintString(str string) {
+	for _, c := range str {
+		ap.PutRune(c)
+	}
+}
